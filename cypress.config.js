a3929c00a692dda1a9e102adcee87e0eb01acdfb
techbabette <marinakrsticrf@gmail.com>
2023-11-17 12:21:46 +0100

    Enable video recording

--- cypress.config.js
+++ cypress.config.js
@@ -4,6 +4,8 @@ module.exports = defineConfig({
   reporter: 'cypress-mochawesome-reporter',
   e2e: {
     baseUrl: "https://magento.softwaretestingboard.com/",
+    video: true,
+    videoCompression: true,
     setupNodeEvents(on, config) {
       require('cypress-mochawesome-reporter/plugin')(on);
     },
